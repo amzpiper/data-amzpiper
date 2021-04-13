faf06d43455e612eb9754e543f2d98cd89727ba9
郭宇航 <1131717341@qq.com>
2021-04-13 17:23:25 +0800

    vue-init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -246,7 +246,7 @@
 <script>
 export default {
   name: "HelloWorld",
-  data() {
+  data () {
     return {
       msg: "Welcome to Your Vue.js App",
       isCollapse: false,
@@ -257,22 +257,22 @@ export default {
         {
           date: "2016-05-02",
           name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
+          address: "上海市普陀区金沙江路 1518 弄"
         },
         {
           date: "2016-05-04",
           name: "王小虎",
-          address: "上海市普陀区金沙江路 1517 弄",
+          address: "上海市普陀区金沙江路 1517 弄"
         },
         {
           date: "2016-05-01",
           name: "王小虎",
-          address: "上海市普陀区金沙江路 1519 弄",
+          address: "上海市普陀区金沙江路 1519 弄"
         },
         {
           date: "2016-05-03",
           name: "王小虎",
-          address: "上海市普陀区金沙江路 1516 弄",
+          address: "上海市普陀区金沙江路 1516 弄"
         }
       ]
     }
