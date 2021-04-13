741a3d1280b4c66cb40f1abad4b672d5113c4eec
郭宇航 <1131717341@qq.com>
2021-04-13 15:58:59 +0800

    vue-init

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import ElementPlus from 'element-plus';
 import 'element-plus/lib/theme-chalk/index.css';
 import App from './App.vue';
 import locale from 'element-plus/lib/locale/lang/zh-cn'
+import Axios from 'axios'
 
 const app = createApp(App)
 app.use(ElementPlus, { size: 'small', zIndex: 3000 });
 createApp(App).use(ElementPlus, { locale })
+Vue.prototype.axios = Axios
 app.mount('#app')
