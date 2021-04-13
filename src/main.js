2d73d67afc5b86bc3011657aa8677303be755cfa
郭宇航 <1131717341@qq.com>
2021-04-13 16:56:10 +0800

    vue-init

--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import locale from 'element-ui/lib/locale/lang/en'
+import axios from 'axios'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI, { locale })
+Vue.prototype.axios = axios
 
 /* eslint-disable no-new */
 new Vue({
